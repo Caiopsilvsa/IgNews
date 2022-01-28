150debaeecba3db382625e87ad9518c8c74c7512
Caiopsilvsa <caio.psilva@outlook.com>
2022-01-28 12:28:59 -0300

    Autenticação GitHub

--- src/components/signInButton/index.tsx
+++ src/components/signInButton/index.tsx
@@ -1,10 +1,11 @@
 import styles from './styles.module.scss'
 import {FaGithub} from 'react-icons/fa';
 import {FiX} from 'react-icons/fi';
+import {signIn,useSession} from 'next-auth/react';
 
 export function SingInButton(){
-    const isUserLogged = true;
-    return isUserLogged ?(
+    const {data:session} = useSession();
+    return session ?(
         <button type="button" className={styles.signInButton}>
         <FaGithub color ="#04d361" />
          Caio Pereira da Silva
@@ -13,8 +14,9 @@ export function SingInButton(){
      </button>
  
         ):(
-            <button type="button" className={styles.signInButton}>
-            <FaGithub color ="#eba417" />
+            <button type="button" className={styles.signInButton}
+            onClick={()=>signIn('github')}>
+            <FaGithub color ="#eba417"/>
              Sign in with Github
             
          </button>
